801a5a491e7df0f4eeb9df7a42610b09416d6a37
Ehab Hamdy <ehab.hussein@minnosphere.com>
2022-08-20 22:08:08 +0200

    Updated prediction model based on linear regression

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from flask.logging import create_logger
 import logging
 
 import pandas as pd
-from sklearn.externals import joblib
+import joblib
+
+
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
+
 
 def scale(payload):
     """Scales Payload"""
@@ -17,6 +20,7 @@ def scale(payload):
     scaler = StandardScaler().fit(payload)
     scaled_adhoc_predict = scaler.transform(payload)
     return scaled_adhoc_predict
+
 
 @app.route("/")
 def home():
@@ -53,12 +57,11 @@ def predict():
     { "prediction": [ 20.35373177134412 ] }
 
     """
-
     try:
-        clf = joblib.load("boston_housing_prediction.joblib")
+      clf = joblib.load("boston_housing_prediction.joblib")
     except:
-        LOG.info("JSON payload: %s json_payload")
-        return "Model not loaded"
+      LOG.info("JSON payload: %s json_payload")
+      return "Model not loaded"
 
     json_payload = request.json
     LOG.info("JSON payload: %s json_payload")
@@ -67,6 +70,7 @@ def predict():
     scaled_payload = scale(inference_payload)
     prediction = list(clf.predict(scaled_payload))
     return jsonify({'prediction': prediction})
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
